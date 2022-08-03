1cb38288610f8401a6a483e5b6ada6992e9b074a
Rostyslav Radysh <rostyslav.radysh@gmail.com>
2022-08-03 23:58:03 +0300

    Add new features

--- src/components/screenSavers/index.tsx
+++ src/components/screenSavers/index.tsx
@@ -4,35 +4,43 @@ import { createPortal } from 'react-dom'
 
 export interface ScreenSaverProps {
     isOpen: boolean
+    color: string
+    isImage: boolean
+    imageUrl: string
     onClick: (value: boolean) => void
 }
 
-export const ScreenSaver: FunctionComponent<PropsWithChildren<ScreenSaverProps>> = ({ isOpen, onClick, children }: PropsWithChildren<ScreenSaverProps>) => isOpen ? (
-    <>
-        {createPortal(<div className="fixed 
-                top-0 
-                left-0 
-                w-full 
-                h-full 
-                flex 
-                items-center 
-                justify-center">
-            <div className="bg-white 
-                    opacity-100 
+export const ScreenSaver: FunctionComponent<PropsWithChildren<ScreenSaverProps>> = ({ isOpen, color, isImage, imageUrl, onClick, children }: PropsWithChildren<ScreenSaverProps>) => {
+    const hex = color.substring(2)
+
+    return isOpen ? (
+        <>
+            {createPortal(<div className="fixed 
+                    top-0 
+                    left-0 
                     w-full 
                     h-full 
-                    z-10" onClick={() => onClick(!isOpen)} />
-                <div className="bg-white
-                        fixed 
-                        w-1/2
-                        h-2/6
-                        z-20">
-                    {children}
-                </div>
-            </div>,
-            document.body
-        )}
-    </>
-) : null
+                    flex 
+                    items-center 
+                    justify-center">
+                <div className={` 
+                        opacity-100 
+                        w-full 
+                        h-full 
+                        z-10
+                        ${isImage ?  `bg-[url(${imageUrl})]` : `bg-[#${hex}]`}`} onClick={() => onClick(!isOpen)} />
+                    <div className="bg-white
+                            fixed 
+                            w-1/2
+                            h-2/6
+                            z-20">
+                        {children}
+                    </div>
+                </div>,
+                document.body
+            )}
+        </>
+    ) : null
+}
 
 export default ScreenSaver
